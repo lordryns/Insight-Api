2a3323ef35faa07a9495edeab0308ee3b78e14df
David <rynstheoverlord@gmail.com>
2024-07-08 08:44:03 +0100

    Made purified string match the original in length!

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ async def purge_text(text: str) -> dict:
     new_text = text
 
     for word in BAD_WORDS:
-        new_text = new_text.replace(word.lower(), "****")
+        new_text = new_text.replace(word.lower(), '*' * len(word))
         
     return {"text": text,  "purified_string": new_text}
