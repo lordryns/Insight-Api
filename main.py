b0316d7940c2506d387167a91f7182d32d48ce36
David <rynstheoverlord@gmail.com>
2024-07-08 08:11:00 +0100

    Connected Json training data 🧨

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+import json 
+
+with open("profanities.json", 'r') as fp:
+    bad_json = json.load(fp)
+
+BAD_WORDS = bad_json["words"]
+
 
 app = FastAPI()
 
@@ -9,4 +16,8 @@ def home():
 
 @app.get("/purify/{text}")
 async def purge_text(text: str) -> dict:
-    return {"text": text, "purified_string": text}
+    new_text = ""
+
+    for words in BAD_WORDS:
+        new_text = text.replace(words, "****")
+    return {"text": text, "purified_string": new_text}
