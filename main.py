e9030c7f0fb2ab2fcfebbbd76407100443bff9e3
David <rynstheoverlord@gmail.com>
2024-07-08 07:57:35 +0100

    Added the purify endpoint.

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"title": "Inisight Api"}
+
+
+@app.get("/purify/{text}")
+async def purge_text(text: str) -> dict:
+    return {"text": text, "purified_string": text}
